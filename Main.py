e33198606844b16d1f767d5f66a52da9c46eed34
omarelharras <75047331+omarelharras@users.noreply.github.com>
2020-12-02 01:00:22 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,17 +2,47 @@ import pyverilog.vparser.ast as vast
 from pyverilog.vparser.parser import parse
 from pyverilog.ast_code_generator.codegen import ASTCodeGenerator
 
-# loop over all items with type InstanceList, look for flip flop cells
-for itemDeclaration in definition.items:
-    item_type = type(itemDeclaration).__name__
-    if item_type == "InstanceList": 
-        if instance.module == sky130_fd_sc_hd__dfrtn or instance.module == sky130_fd_sc_hd__dfrbp or instance.module == sky130_fd_sc_hd__dfrtp or instance.module == sky130_fd_sc_hd__dfxtp 
-        
+FlipFlopList = [sky130_fd_sc_hd__dfrtn, sky130_fd_sc_hd__dfrbp, sky130_fd_sc_hd__dfrtp,sky130_fd_sc_hd__dfxtp]
+MuxList = [sky130_fd_sc_hd__mux2, sky130_fd_sc_hd__mux2i, sky130_fd_sc_hd__mux4]
+NewItemList = []
+WireList = []
+MuxInstanceList = []
 
 statements = []
 
-rtl = "spm.v"
+rtl = sys.argv[1] # get gate level netlist file name from user
 ast, _ = parse([rtl])
+
+Instances = []
+
+# loop over all items with type InstanceList, look for flip flop cells and get the wire names that comes from the multiplexer
+for itemDeclaration in definition.items:
+    item_type = type(itemDeclaration).__name__
+    if item_type == "InstanceList": 
+        if instance.module in FlipFlopList:
+            for hook in instance.portlist:
+                if hook.portname == "D":
+                    WireList.append(hook.argname)
+
+
+# loop over all items with type InstanceList, look for multiplexor cells whose output wire go into flip flop
+for itemDeclaration in definition.items:
+    item_type = type(itemDeclaration).__name__
+    if item_type == "InstanceList":
+    if instance.module in MuxList:
+            for hook in instance.portlist:
+                if hook.argname in WireList:
+                    MuxInstanceList.append(instance.name)
+
+# loop over all items with type InstanceList, only add cells that aren't multiplexors connected to flip flops       
+for itemDeclaration in definition.items:
+    item_type = type(itemDeclaration).__name__
+    if item_type == "InstanceList":
+        if instance.module not in MuxInstanceList:
+            NewItemList.append(instance)
+
+
+
 
 
 clock_gate_args = 
@@ -29,3 +59,12 @@ clock_gate_cell = vast.Instance
     tuple(clock_gate_args),
     tuple()
 )
+
+instances.append(vast.InstanceList("sky130_fd_sc_hd__dlclkp", tuple(), tuple([clock_gate_cell])))
+
+for itemDeclaration in definition.items:
+    item_type = type(itemDeclaration).__name__
+    if item_type == "InstanceList": 
+        if instance.module in FlipFlopList:
+            if hook.portname == "En":
+                hook.argname = "__clock_gate_output_c_"
