ae08461ca94775517d6ff7cb76a458db383f1048
Hoda Ahmed <75049279+hodaAhmed97@users.noreply.github.com>
2020-12-02 21:11:25 +0000

    Add files via upload

---
+++ Test1.v
@@ -0,0 +1,8 @@
+`timescale 1ns / 1ps
+module Test1(input CLK, input EN, input D_IN, output reg D_OUT);
+
+    always@(posedge CLK) begin
+        if(EN)
+            D_OUT <= D_IN;
+    end
+endmodule
